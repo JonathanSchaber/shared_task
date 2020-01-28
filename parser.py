1e9db5fcd1e43fabe75b9e62c22299a7d17497a5
Janis Goldzycher <janisgoldzycher@mailbox.org>
2020-01-28 16:16:46 +0100

    add parser file

---
+++ parser.py
@@ -0,0 +1,16 @@
+class NoahParser:    
+
+    def __init__(self, path_in, path_out):
+        pass
+
+
+class ChatmaniaParser:
+
+    def __init__(self, path_in, path_out):
+        pass
+
+
+class SwissCrawlParser:
+    
+    def __init__(self, path_in, path_out):
+        pass
