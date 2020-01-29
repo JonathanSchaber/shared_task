8b0eb56c4358cea746bb60af97d8a6e3e1fc3c78
Jonathan Schaber <jonathan.schaber@uzh.ch>
2020-01-29 08:31:44 +0100

    implement base class from which other classes inherit

--- parser.py
+++ parser.py
@@ -1,16 +1,20 @@
-class NoahParser:    
-
-    def __init__(self, path_in, path_out):
-        pass
-
-
-class ChatmaniaParser:
-
-    def __init__(self, path_in, path_out):
-        pass
-
-
-class SwissCrawlParser:
+class Parser:
     
     def __init__(self, path_in, path_out):
+        self.path_in = path_in
+        self.path_out = path_out
+
+    @static_method
+    def load_file():
         pass
+
+
+class NoahParser(Parser): 
+    pass
+
+class ChatmaniaParser(Parser):
+    pass
+
+class SwissCrawlParser(Parser):
+    pass 
+
