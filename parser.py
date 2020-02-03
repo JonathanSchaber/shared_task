a2155045100134aa7313a5db43fc8e4fc6d84b3e
Janis Goldzycher <janisgoldzycher@mailbox.org>
2020-02-03 13:20:58 +0100

    last parser update

--- parser.py
+++ parser.py
@@ -1,11 +1,24 @@
+"""
+p = NoahParser(path_in, path_out)
+p.load_file()
+p.copy_to_main_file()
+
+
+"""
+
+
 class Parser:
     
     def __init__(self, path_in, path_out):
         self.path_in = path_in
         self.path_out = path_out
 
-    @staticmethod
-    def load_file():
+    def load_file(self):
+        """Open file without reading for memory restriction reasons."""
+        raise NotImplementedError
+
+    def copy_to_main_file(self):
+        """Copy the loaded file to the main file."""
         raise NotImplementedError
 
 
@@ -17,6 +30,7 @@ class ChatmaniaParser(Parser):
 
 class SwissCrawlParser(Parser):
     pass
+
 
 p = Parser('', '')
 p.load_file()
