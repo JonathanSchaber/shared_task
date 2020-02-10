c77cf995d33b1b6703a385a3b2ce054c80519349
Janis Goldzycher <janisgoldzycher@mailbox.org>
2020-02-10 14:14:06 +0100

    write swisscrawl parser

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
+import csv
+
 """
 p = NoahParser(path_in, path_out)
-p.load_file()
 p.copy_to_main_file()
 
 
@@ -9,13 +10,9 @@ p.copy_to_main_file()
 
 class Parser:
     
-    def __init__(self, path_in, path_out):
+    def __init__(self, path_in):
         self.path_in = path_in
-        self.path_out = path_out
-
-    def load_file(self):
-        """Open file without reading for memory restriction reasons."""
-        raise NotImplementedError
+        self.infile = open(self.path_in, 'r', encoding='utf8')
 
     def copy_to_main_file(self):
         """Copy the loaded file to the main file."""
@@ -25,12 +22,34 @@ class Parser:
 class NoahParser(Parser): 
     pass
 
+
 class ChatmaniaParser(Parser):
     pass
 
+
 class SwissCrawlParser(Parser):
-    pass
+
+    name = 'swisscrawl'
+    num_lines = 562525
+    out_path = 'data/main/swisscrawl_parsed.csv'
+
+    def copy_to_main_file(self):
+        """Copy the loaded file to the main file."""
+        reader = csv.reader(self.infile)
+        writer = csv.writer(open(self.out_path, 'w', encoding='utf8'))
+        for i, row in enumerate(reader):
+            if i == 0:
+                continue
+            writer.writerow([row, '1', self.name])
+            if i % 10000:
+                print('Processed line {} of {}.'.format(i+1, self.num_lines))
 
 
-p = Parser('', '')
-p.load_file()
+def main():
+    path_in = 'data/swisscrawl/swisscrawl-2019-11-23.csv'
+    p = SwissCrawlParser(path_in)
+    p.copy_to_main_file()
+
+
+if __name__ == '__main__':
+    main()
