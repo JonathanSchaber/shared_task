ab171b9c58704fbf1061b4ebe6cc15044a347d65
Jonathan Schaber <jonathan.schaber@uzh.ch>
2020-03-11 15:04:10 +0100

    computing accuracy for different granularities implemented

--- evaluation.py
+++ evaluation.py
@@ -27,19 +27,43 @@ def process_predictions_file(pred_file):
     true_fine = 0
     false_fine = 0
 
+    TER = False
+    FINE = False
+
     with open(pred_file, 'r', encoding='utf-8') as f:
         csv_reader = csv.reader(f)
         for line in csv_reader:
             text_id, label_binary, label_ternary, label_finegrained, pred_binary, pred_ternary, pred_finegrained, text, masked, source = line
-            if label_binary == pred_binary: true_bin += 1 else: false_bin += 1
+            if label_binary == pred_binary: true_bin += 1 
+            else: false_bin += 1
             if pred_ternary == "NULL": 
                 continue
             else:
-                if label_ternary == pred_ternary: true_ter += 1 else: false_ter += 1
+                TER = True
+                if label_ternary == pred_ternary: true_ter += 1 
+                else: false_ter += 1
                 if pred_finegrained == "NULL":
                     continue
                 else:
-                    if label_finegrained == pred_finegrained: true_fine += 1 else: false_ter += 1
+                    FINE = True
+                    if label_finegrained == pred_finegrained: true_fine += 1 
+                    else: false_ter += 1
+
+    if true_bin != 0:
+        print('Accuracy binary: {}'.format((true_bin/(true_bin + false_bin)*100)))
+    else:
+        print('No correct binary predictions...')
+    if TER:
+        if true_ter != 0:
+            print('Accuracy ternary: {}'.format((true_ter/(true_ter + false_ter)*100)))
+        else:
+            print('No correct ternary predictions...')
+    if FINE:
+        if true_fine != 0:
+            print('Accuracy finegrained: {}'.format((true_fine/(true_fine + false_fine)*100)))
+        else:
+            print('No correct finegrained predictions...')
+    
 
 def compare_line_by_line(gold_path, predicted_path):
     """Compares the gold lables to the predicted ones.
@@ -93,8 +117,9 @@ def write_measures_to_file(true_pos, false_pos, true_neg, false_neg, out_file):
 
 def main():
     args = parse_cmd_args()
-    true_pos, false_pos, true_neg, false_neg = compare_line_by_line(args.gold_path, args.predicted_path)
-    write_measures_to_file(true_pos, false_pos, true_neg, false_neg, args.path_out)
+    #true_pos, false_pos, true_neg, false_neg = compare_line_by_line(args.gold_path, args.predicted_path)
+    #write_measures_to_file(true_pos, false_pos, true_neg, false_neg, args.path_out)
+    process_predictions_file(args.predicted_path)
 
 
 if __name__ == '__main__':
