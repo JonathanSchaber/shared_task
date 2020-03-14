ba7b64ec48d4f243ae0cf506831c705635674533
Janis Goldzycher <janisgoldzycher@mailbox.org>
2020-03-14 14:52:19 +0100

    add func to count num classes

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import csv
 import datetime
 import re
 
@@ -5,3 +6,19 @@ import re
 def get_timestamp():
     timestamp = datetime.datetime.now().strftime('%c')
     return re.sub(r'\s+', r'_', timestamp)
+
+
+def count_num_classes(path_in, idx):
+    """Count the number of classes.
+
+    Args:
+        path_in: str, path to csv
+        idx: int, index of row where class label is
+    """
+    labels = set()
+    with open(path_in, 'r', encoding='utf8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            label = row[idx]
+            labels.add(label)
+    return len(labels)
